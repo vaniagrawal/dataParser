22c6617f44964ec930a338ec392b2a158ad55913
vaniagrawal <vagrawal047@student.fuhsd.org>
2019-03-04 22:04:04 -0800

    add Arraylist to return the values returned from the Utils.java class

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,11 @@
  * Main class for data parser
  * @Author: Vani Agrawal
  */
+import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         String data = Utils.readFileAsString("data/2016_Presidential_Results.csv");
-        System.out.println(data);
+        ArrayList<ElectionResult> results = Utils.parse2018ElectionResults(data);
 
     }
 }
