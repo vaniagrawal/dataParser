d0e01d3b0a2779b3f643785a3c1611db12535ee7
vaniagrawal <vagrawal047@student.fuhsd.org>
2019-03-04 21:40:33 -0800

    create a replaceAll method and fill in the parse2018ElectionResults method

--- src/Utils.java
+++ src/Utils.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
+import static com.sun.org.apache.xml.internal.security.keys.keyresolver.KeyResolver.length;
+
 public class Utils {
     public static String readFileAsString(String filepath) {
         StringBuilder output = new StringBuilder();
@@ -24,8 +26,43 @@ public class Utils {
     public static ArrayList<ElectionResult> parse2018ElectionResults(String data) {
         ArrayList<ElectionResult> result = new ArrayList<>();
         String[] lines = data.split("\n");
+        String[] linesArray;
+
+        for (int i = 1; i < lines.length; i++) {
+
+
+            linesArray = lines[i].split(",");
+
+
+            double votes_dem = Double.parseDouble(linesArray[1]);
+            double votes_gop = Double.parseDouble(linesArray[2]);
+            double total_votes = Double.parseDouble(linesArray[3]);
+            double per_dem = Double.parseDouble(linesArray[4]);
+            double per_gop = Double.parseDouble(linesArray[5]);
+            String diffString = replaceAll(",",linesArray[6]);
+            double diff = Double.parseDouble(diffString);
+            String per_diff_String = replaceAll("%",linesArray[7]);
+            double per_point_diff = Double.parseDouble(per_diff_String);
+            String state_abbr = linesArray[8];
+            String county_name = linesArray[9];
+            double combined_fips = Double.parseDouble(linesArray[10]);
+            ElectionResult electionResultObject = new ElectionResult(votes_dem, votes_gop, total_votes, per_dem, per_gop, diff, per_point_diff, state_abbr, county_name, combined_fips);
+            result.add(electionResultObject);
+
+
+        }
 
 
         return result;
+    }
+    public static String replaceAll(String replacedThing, String word ){
+        int indexOFReplacer = word.indexOf(",", word.indexOf("\""));
+        String wordWithoutReplacedThing = "";
+        if(indexOFReplacer != -1){
+
+            wordWithoutReplacedThing = word.substring(0, indexOFReplacer)+ word.substring(indexOFReplacer +1, word.length());
+
+        }
+        return wordWithoutReplacedThing;
     }
 }
